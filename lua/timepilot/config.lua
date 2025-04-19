7802a84259d08210577958aca9acfa2badd34db2
frenki <frenki@email.com>
2025-04-19 23:30:18 +0200

    Instalation config 3

--- lua/timepilot/config.lua
+++ lua/timepilot/config.lua
@@ -1,48 +1,16 @@
-local M ={}
-
-local function install_timepilot()
-    if M.config.autoinstall then
-        return os.execute("go install github.com/frenki123/timepilot.nvim/timepilot@latest")
-    end
-    return false
-end
-
-function M.build()
-    local required_version = "0.0.1"
-    local timepilot = M.config.timepilot_path
-    local handle = io.popen(timepilot .. "version")
-    if not handle then
-        if not install_timepilot() then
-            print("Timepilot not installed")
-            return false
-        end
-        return
-    end
-    local result = handle:read("*a")
-    handle:close()
-    local version = result:match("v(.+)")
-    if version == required_version then
-        return
-    end
-    if not install_timepilot() then
-        print("Required Timepilot version is" .. required_version .. "installed" .. version)
-    end
-end
-
+local M = {}
 
 M.config = {
-    timepilot_path = "timepilot",
-    autoinstall = true
+  timepilot_path = "timepilot",
+  autoinstall = true,
 }
 
-
 function M.set_config(user_config)
-    for option, value in pairs(user_config) do
-        M.config[option] = value
-    end
+  for option, value in pairs(user_config) do
+    M.config[option] = value
+  end
 end
 
-function M.check_config()
-end
+function M.check_config() end
 
 return M
--- lua/timepilot/init.lua
+++ lua/timepilot/init.lua
@@ -2,8 +2,33 @@ local M = {}
 local client = require("timepilot.client")
 local config = require("timepilot.config")
 
+local function install_timepilot()
+  if M.config.autoinstall then
+    return os.execute("go install github.com/frenki123/timepilot.nvim/timepilot@latest")
+  end
+  return false
+end
+
 function M.build()
-    config.build()
+  local required_version = "0.0.1"
+  local timepilot = M.config.timepilot_path
+  local handle = io.popen(timepilot .. " version")
+  if not handle then
+    if not install_timepilot() then
+      print("Timepilot not installed")
+      return false
+    end
+    return
+  end
+  local result = handle:read("*a")
+  handle:close()
+  local version = result:match("v(.+)")
+  if version == required_version then
+    return
+  end
+  if not install_timepilot() then
+    print("Required Timepilot version is" .. required_version .. "installed" .. version)
+  end
 end
 
 function M.setup(user_config)
